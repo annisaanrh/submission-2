a77137e5ac49e7df4ecf03ec81945b6838799553
Annisaa Nuur Huwaidah <annisaa.nh@gmail.com>
2020-01-04 23:53:52 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -19,7 +19,7 @@ function error(error) {
   // Parameter error berasal dari Promise.reject()
   console.log("Error : " + error);
 }
-var request = new Request(base_url + "competitions/2002/standings", {
+var request = new Request(base_url + "/v2/competitions/2002/standings", {
     headers: new Headers({
         'X-Auth-Token' : '37209e11774845f298285ae0df906655'
     })
@@ -41,23 +41,43 @@ fetch(base_url, {
 })
     .then(res => res.json())
     .then(function(data) {
-
       // Objek/array JavaScript dari response.json() masuk lewat data.
+    console.log(data);
     // Menyusun komponen card artikel secara dinamis
     var articlesHTML = "";
-    data.standings.forEach(function(team){
+    data.standings["0"].table.forEach(function(item){
         articlesHTML += `
-              <div class="card">
-                <a href="./article.html?id=${team.id}">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <img src="${team.name}" />
-                  </div>
-                </a>
-                <div class="card-content">
-                  <span class="card-title truncate">${team.shortName}</span>
-                  <p>${team.address}</p>
-                </div>
-              </div>
+        <table style="font-size:16px center;">
+        <thead>
+            <tr> 
+                <th>Team</th>
+                <th></th>
+                <th>Draw</th>  
+                <th>Goal Difference</th>
+                <th>Goal Against</th>  
+                <th>Lost</th>
+                <th>Played Games</th>  
+                <th>Points</th>
+                <th>Position</th>  
+                <th>Won</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${item.team.name}</td>
+                <td style><img style="width:50px;" src="${item.team.crestUrI}"></td>
+                <td>${item.draw}</td>
+                <td>${item.goalDifference}</td>
+                <td>${item.goalsAgainst}</td>
+                <td>${item.goalsFor}</td>
+                <td>${item.lost}</td>
+                <td>${item.playedGames}</td>
+                <td>${item.points}</td>
+                <td>${item.position}</td>
+                <td>${item.won}</td>            
+            </tr>
+        </tbody>
+        </table>
             `;
     });
     // Sisipkan komponen card ke dalam elemen dengan id #content
